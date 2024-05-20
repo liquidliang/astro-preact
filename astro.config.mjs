bbbe082c43bb82b78d854bd9f0cc8b949654228e
liquidliang <ljquan@qq.com>
2024-05-20 17:28:50 +0800

    视图过度

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import preact from "@astrojs/preact";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://astro-liquid.liquidliang.cc',
+  integrations: [mdx(), sitemap(), preact(), vue()]
 });
---
+++ src/components/Greeting.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <h3>{{ greeting }}! Thank you for visiting!</h3>
+      <button @click="generateRandomGreeting">
+        New Greeting
+      </button>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const props = defineProps({
+    messages: Array
+  });
+  
+  const greeting = ref(props.messages[0]);
+  
+  function generateRandomGreeting() {
+    const randomIndex = Math.floor(Math.random() * props.messages.length);
+    greeting.value = props.messages[randomIndex];
+  }
+  </script>
